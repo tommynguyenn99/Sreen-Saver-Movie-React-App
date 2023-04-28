9bf95f2294abb59a1462504267006cc3ab42507f
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-04-27 19:05:04 -0700

    components push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import React from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-
-      </header>
+      <React.Fragment>
+        <Navbar/>
+        <SearchBar/>
+      </React.Fragment>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
