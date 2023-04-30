73d66cafa124096fa754b8daf8448fbf897ff84c
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-04-30 09:47:19 -0700

    started hero image

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -1,6 +1,39 @@
-function PageHome() {
-    return (
-      <section className="home-page">Page Home</section>
-    )
-  }
-  export default PageHome
+import React, { useState, useEffect } from 'react';
+import { API_URL, API_IMG } from '../components/globals/globals';
+import MovieCard from './MovieCard';
+
+function PageHome(props) {
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => {
+        setMovies(data.results);
+        setSelectedMovie(data.results[0]); // Select the first movie as the default
+      });
+  }, []);
+
+  return (
+    <div className="body">
+      {selectedMovie && (
+        <div className="hero">
+          <img src={API_IMG + selectedMovie.poster_path} alt={selectedMovie.title} />
+          <div className="hero-content">
+            <p className='hero-description'>{selectedMovie.overview}</p>
+          </div>
+        </div>
+      )}
+      <div className="container-fluid">
+        <div className="row">
+          {movies.map(movie => (
+            <MovieCard key={movie.id} {...movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageHome;
