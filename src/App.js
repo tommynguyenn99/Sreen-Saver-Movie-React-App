bc974a9adc0f1771ad014f19dec3b1124caac0c5
aintright2 <122578506+aintright2@users.noreply.github.com>
2023-05-01 01:47:29 -0700

    imported Header/Footer/Navbar into App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageHome from "./components/PageHome";
 import PageSingleMovie from "./components/PageSingleMovie";
+import PageAbout from "./components/PageAbout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -18,10 +22,14 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Header />
+      <Navbar />
       <Routes>
+        <Route path="/about" exact element={<PageAbout />} />
         <Route path="/movie/:id" element={<PageSingleMovie />} />
         <Route path="/" exact element={<PageHome />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
