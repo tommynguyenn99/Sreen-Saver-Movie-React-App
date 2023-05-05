0f47fd7bf37ea485efd6b5eea3b00043ef0bea25
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-05-04 22:06:20 -0700

    added correct movie when clicked and trailer

--- src/components/globals/globals.js
+++ src/components/globals/globals.js
@@ -1,12 +1,23 @@
+// API KEY,TOKENS, IMPORTANT INFO
 export const API_KEY = "e3ff3458772dcbd41d3f954d2f21c122";
 export const API_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2ZmMzQ1ODc3MmRjYmQ0MWQzZjk1NGQyZjIxYzEyMiIsInN1YiI6IjY0NGMxNmM4NzI2ZmIxMDVjODA2M2NmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DJw24rHXnxoIEII3NC7UGA2a28G01g_cRepFsIv0NSg";
 export const APP_FOLDER_NAME = "movie-react-app";
 export const API_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=e3ff3458772dcbd41d3f954d2f21c122&language=en-US&page=1";
+
+// IMAGES
 export const API_IMG = "https://image.tmdb.org/t/p/w500/";
 export const API_IMG_BACKDROP = "https://image.tmdb.org/t/p/original/";
 export const API_BASE_URL = "https://api.themoviedb.org/3/movie";
+
+// SINGLE PAGE IMAGE
+export const API_IMG_SINGLE_POSTER = "https://image.tmdb.org/t/p/w154/";
+export const API_IMG_SINGLE_BACKDROP = "https://image.tmdb.org/t/p/w1280/";
+export const API_MOVIE_RUNTIME =
+  "https://api.themoviedb.org/3/movie/550?api_key=e3ff3458772dcbd41d3f954d2f21c122";
+
+export const API_MOVIE_TRAILER = `https://api.themoviedb.org/3/movie/$/videos?api_key=e3ff3458772dcbd41d3f954d2f21c122&language=en-US`;
 
 export const LOGO =
   "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg";
