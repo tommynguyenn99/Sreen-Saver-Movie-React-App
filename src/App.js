aa32a4d419442849d64bf123517ea355bb40d250
Tommy Nguyen <tommy.tuannguyen@outlook.com>
2023-05-11 21:20:43 -0700

    single movie page complete

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import "./styles/styles.scss";
 
 function App() {
+  // need to change tab name to movie title
   return (
     <BrowserRouter>
       <Header />
