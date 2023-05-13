6a519cd8b63c27545dff271ba5a8930d7517f7cf
aintright2 <122578506+aintright2@users.noreply.github.com>
2023-05-12 21:19:51 -0700

    adding category selector

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -4,21 +4,30 @@ import {
   API_URL,
   API_IMG,
   API_IMG_BACKDROP,
+  buildApiUrl,
 } from "../components/globals/globals";
 import MovieCard from "./MovieCard";
 import "../styles/styles.scss";
 
+const categories = [
+  { id: "popular", label: "Popular" },
+  { id: "now_playing", label: "Now Playing" },
+  { id: "top_rated", label: "Top Rated" },
+  { id: "upcoming", label: "Upcoming" },
+];
+
 function PageHome(props) {
   const [movies, setMovies] = useState([]);
   const [selectedMovieIndex, setSelectedMovieIndex] = useState(0);
-
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
   useEffect(() => {
-    fetch(API_URL)
+    const apiUrl = buildApiUrl(selectedCategory.id);
+    fetch(apiUrl)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
       });
-  }, []);
+  }, [selectedCategory]);
 
   const handleNext = () => {
     setSelectedMovieIndex((prevIndex) => (prevIndex + 1) % movies.length);
@@ -92,6 +101,18 @@ function PageHome(props) {
               ></div>
             ))}
           </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          {categories.map((category) => (
+            <button
+              key={category.id}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category.label}
+            </button>
+          ))}
         </div>
       </div>
       <div className="container-fluid">
--- src/components/globals/globals.js
+++ src/components/globals/globals.js
@@ -1,11 +1,15 @@
 // API KEY,TOKENS, IMPORTANT INFO
 export const API_KEY = "e3ff3458772dcbd41d3f954d2f21c122";
+export const API_KEY_PARAM = `api_key=${API_KEY}`;
 export const API_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlM2ZmMzQ1ODc3MmRjYmQ0MWQzZjk1NGQyZjIxYzEyMiIsInN1YiI6IjY0NGMxNmM4NzI2ZmIxMDVjODA2M2NmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DJw24rHXnxoIEII3NC7UGA2a28G01g_cRepFsIv0NSg";
 export const APP_FOLDER_NAME = "movie-react-app";
+export const API_LANGUAGE = "en-US";
+export const API_LANGUAGE_PARAM = `&language=${API_LANGUAGE}`;
+export const API_PAGE_DEFAULT = "1";
+export const API_PAGE_DEFAULT_PARAM = `&page=${API_PAGE_DEFAULT}`;
 export const API_URL =
   "https://api.themoviedb.org/3/movie/popular?api_key=e3ff3458772dcbd41d3f954d2f21c122&language=en-US&page=1";
-
 // IMAGES
 // Use this for posters
 export const API_IMG = "https://image.tmdb.org/t/p/w500/";
@@ -28,3 +32,11 @@ export const LOGO =
   "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg";
 // Add api image for poster and backdrop
 // backdrop in diff size
+
+/**
+ *
+ * @param {string} endpoint
+ * @returns string full api endpoint
+ */
+export const buildApiUrl = (endpoint = "popular") =>
+  `${API_BASE_URL}/${endpoint}?${API_KEY_PARAM}${API_LANGUAGE_PARAM}${API_PAGE_DEFAULT_PARAM}`;
